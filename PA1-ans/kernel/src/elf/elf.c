af698ce49f9c86a47e8903392fb67b8f89ae2092
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-13 12:44:53 +0000

    >  compile NEMU 3019205474 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:44:53 up 4 days, 22:35,  1 user,  load average: 0.61, 0.74, 0.77 2d08e7bcdf72e4cce7f57053540d7c8b5eebfb62

--- PA1-ans/kernel/src/elf/elf.c
+++ PA1-ans/kernel/src/elf/elf.c
@@ -29,7 +29,7 @@ uint32_t loader() {
 #endif
 
 	elf = (void*)buf;
-	ph = (void*)(buf + elf -> e_phoff);
+//	ph = (void*)(buf + elf -> e_phoff);
 	/* TODO: fix the magic number with the correct one */
 	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
@@ -46,14 +46,14 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			ramdisk_read((void*)(ph -> p_vaddr),ph -> p_offset,ph -> p_filesz);
+			ramdisk_read((uint8_t*)(ph -> p_vaddr),ph -> p_offset,ph -> p_filesz);
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			memset((void*)(ph -> p_vaddr + ph -> p_filesz),0,ph -> p_memsz - ph -> p_filesz);
+			memset((uint8_t*)(ph -> p_vaddr + ph -> p_filesz),0,ph -> p_memsz - ph -> p_filesz);
 
-			ph =ph+1;
+			ph++;
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
